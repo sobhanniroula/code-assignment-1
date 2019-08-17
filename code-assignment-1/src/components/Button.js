72f70e5dc89065d1744ee6e7f0abefed59cd9489
sobhanniroula <sobhan_niroula@yahoo.com>
2019-08-17 09:07:57 +0300

    Button component start

---
+++ code-assignment-1/src/components/Button.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './Button.css';
+
+
+
+export default class Button extends Component {
+    render() {
+        return (
+            <div className="button">
+                
+            </div>
+        )
+    }
+}
