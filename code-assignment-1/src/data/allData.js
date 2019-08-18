b061ccdd2b14375322abc389c700a0deca9fbafb
sobhanniroula <sobhan_niroula@yahoo.com>
2019-08-18 13:40:50 +0300

    data js file

---
+++ code-assignment-1/src/data/allData.js
@@ -0,0 +1,20 @@
+const cart = {
+    "totalItems":17,
+    "totalPrice":"338.00",
+    "items":[
+        {
+            "name":"Bag",
+            "price":"167.00",
+            "qty":6,
+            "imgSrc":"img\/products\/bag.jpg"},
+            {"name":"Scarf",
+            "price":"405.00",
+            "qty":2,
+            "imgSrc":"img\/products\/scarf.jpg"
+        }
+    ]
+}
+
+export default {
+    cart: cart,
+}
