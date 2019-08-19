9e5541edda3b748d988db7bfae4500174caa1918
sobhanniroula <sobhan_niroula@yahoo.com>
2019-08-19 03:01:56 +0300

    newsletter subscription status implementation

--- php-files/public/src/js/components/Footer.jsx
+++ php-files/public/src/js/components/Footer.jsx
@@ -5,7 +5,9 @@ export default class Footer extends Component {
         super(props)
         this.state = { 
             email: "",
+            isLoading: false,
             isValidated: false,
+            status: ""
         };
 
     }
@@ -17,9 +19,13 @@ export default class Footer extends Component {
     submitEmail(event) {
         this.validateEmail();
         this.setState({email: ""});
+
+        const makeBlock = document.querySelector('.subscription-validate');
+        makeBlock.style.display = "block";
     }
 
     validateEmail() {
+        this.setState({ isLoading: true });
         const email = this.state.email;
         fetch(`/newsletter/subscribe?email=${email}`, {
             method: "GET",
@@ -27,19 +33,49 @@ export default class Footer extends Component {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
               }
-            
         }).then(response => response.json())
         .then(data => {
-            console.log(data.status);
-            this.setState({
-                isValidated: true
-            });
+            setTimeout(() => {
+                this.setState({
+                    isLoading: false,
+                    isValidated: true,
+                    status: data.status
+                });
+            }, 1500)
         }).catch((e) => {
             console.log(e);
+            this.setState({ isLoading: false });
         })
     }
 
     render() {
+
+        const { isLoading, status } = this.state;
+        let subsValidate, statusString;
+
+        if(isLoading) { 
+            subsValidate =
+                    (<div className="loading">
+                        <img src="/img/spinner.svg" alt="spinner"/>
+                        <em> Subscribing to newsletter...</em>
+                    </div>);
+        }
+        
+        if(status === "success") {
+            statusString = (
+                <div className="status-success">
+                    <p><i class="fas fa-check"></i><em> Subscription successful.</em></p>
+                </div> 
+            );
+        } else if(status === "failed") {
+            statusString = (
+                <div className="status-failed">
+                    <p><i class="fas fa-exclamation-triangle"></i><em> Email verfication failed...</em></p>
+                </div>
+            );
+        }
+        
+
         return (
             <div className="footer">
                 <div class="container">
@@ -67,8 +103,12 @@ export default class Footer extends Component {
                             <div className="input-group">
                                 <i class="far fa-envelope subs-icon"></i>
                                 <input type="email" value={this.state.email} onChange = {this.handleChange.bind(this)} className="form-control subs-input" placeholder="Enter your email address" />
-                                <button className="btn btn-subscribe" type="submit" onClick = {this.submitEmail.bind(this)} >Subscribe</button>
+                                <button className="btn btn-subscribe" type="submit" onClick = {this.submitEmail.bind(this)} >Subscribe</button>                                
 	                        </div>
+                            <div className="subscription-validate">
+                                    {subsValidate}
+                                    {statusString}                     
+                            </div>
                         </div>
                     </div>
                 </div>
--- php-files/resources/views/index.blade.php
+++ php-files/resources/views/index.blade.php
@@ -25,16 +25,24 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
+        <!-- Google fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
 
         <title>FrontEnd</title>
     </head>
 
-    <body>
-       <!-- <?php var_dump($products) ?> -->
 
+    <body>
+       
         <div id="root"></div>
+
+        <!-- javascript files -->
         <script type="text/javascript"></script>
         <script  src="dist/main.js"></script>
+
+        <!-- Font Awesome Icons -->
+        <script src="https://kit.fontawesome.com/536d28d1c6.js"></script>
+        
     </body>
 </html>
     
