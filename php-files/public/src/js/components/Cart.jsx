04f2ee5e4642c96676f0d5deec35401727b4a2a2
sobhanniroula <sobhan_niroula@yahoo.com>
2019-08-20 04:21:23 +0300

    pass props from Cart component to CartHover component

--- php-files/public/src/js/components/Cart.jsx
+++ php-files/public/src/js/components/Cart.jsx
@@ -3,15 +3,45 @@ import CartHover from './CartHover.jsx';
 
 
 export default class Cart extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+            totalItems: 0,
+            totalPrice: 0
+        };
+    }
+
+    componentDidMount() {
+        fetch('/cart/get')
+            .then(response => {
+            return response.json();
+        }).then((data) => {
+            this.setState({
+                items: data.items,
+                totalItems: data.totalItems,
+                totalPrice: data.totalPrice
+            });
+        }).catch((e) => console.log(e))
+    }
+
+
     render() {
+        const { items, totalItems, totalPrice } = this.state;
+
         return (
             <div className="cart">
                 <h6 className="cart-icon"><i className="fas fa-shopping-cart"></i></h6>
                 <p className="cart-desc">
-                    <span className="cart-items-count">3</span> items in your cart <span className="cart-total-amount">€75.00</span>
+                    <span className="cart-items-count">{totalItems}</span> items in your cart <span className="cart-total-amount">€{totalPrice}</span>
                 </p>
-                <CartHover />                
+
+                <CartHover 
+                    items={items}
+                />
+                                                        
             </div>
-        )
+        );
     }
 }
--- php-files/public/src/js/components/CartHover.jsx
+++ php-files/public/src/js/components/CartHover.jsx
@@ -1,47 +1,28 @@
 import React, { Component } from 'react';
 
 export default class CartHover extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            items: [],
-            count: 1
-        }
-    }
-
-    componentDidMount() {
-        fetch('/cart/get')
-            .then(response => {
-            return response.json();
-        }).then((data) => {
-            this.setState({
-                items: data.items
-            });
-        }).catch((e) => console.log(e))
-    }
-
-    
+        
     render() {
-        const { items, count } = this.state;
+        const { items } = this.props;
 
         return (
-            <div className="cart-hover">
+            <div className="cart-hover">  
+                
                 {
                     items.map((val) => {
-                        return (     
-                            <div className="row">
+                        return (   
+                            <div className="row">  
                                 <div className="col-2 cart-item">
                                     <img src={val.imgSrc} alt={val.name}/>
                                 </div>
                                 <div className="col-8 cart-item-price">
                                     <h6>{val.name}</h6>
-                                    <p>{count}&times;€{val.price}</p>
+                                    <p>{val.qty}&times;€{val.price}</p>
                                 </div>
                                 <div className="col-2 ml-auto cart-delete">
                                     <a href="">&times;</a>
                                 </div>
-                            </div>
+                            </div> 
                         );
                     })        
                 }
